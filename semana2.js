7fd9b43205e2de9d9fa3e6985d7540fc5d523c3a
juan <jfhermoza@pucesi.edu.ec>
2024-11-01 23:47:06 -0500

    final

--- semana2.js
+++ semana2.js
@@ -4,18 +4,22 @@ function calcularEdad() {
     let edadAnios = fechaActual.getFullYear() - fechaNacimiento.getFullYear();
     let edadMeses = fechaActual.getMonth() - fechaNacimiento.getMonth();
     let edadDias = fechaActual.getDate() - fechaNacimiento.getDate();
-    if (edadMeses < 0 || (edadMeses === 0 && edadDias < 0)) {
-        edadAnios--;
+    if (edadDias < 0) {
+        edadMeses--;
+        const ultimoMes = new Date(fechaActual.getFullYear(), fechaActual.getMonth(), 0);
+        edadDias += ultimoMes.getDate();
     }
-    const totalDias = Math.floor((fechaActual - fechaNacimiento) / (1000 * 60 * 60 * 24));
+    if (edadMeses < 0) {
+        edadAnios--;
+        edadMeses += 12; 
+    }
     const totalHoras = Math.floor((fechaActual - fechaNacimiento) / (1000 * 60 * 60));
     document.getElementById('resultadoEdad').innerText = 
-        `Han pasado ${edadAnios} años, ${totalDias} días y ${totalHoras} horas desde la fecha de nacimiento.`;
+        `Han pasado ${edadAnios} años, ${edadMeses} meses y ${edadDias} días, que es ${totalHoras} horas desde la fecha de nacimiento.`;
 }
 function verificarCedula() {
     const cedula = document.getElementsByName('cedula')[0].value;
-    const regexCedula = /^[0-9]{10}$/; 
-
+    const regexCedula = /^[0-9]{10}$/;
     if (regexCedula.test(cedula)) {
         document.getElementById('resultadoCedula').innerText = "La cédula es válida.";
     } else {
